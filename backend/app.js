406dd578d87773663ffd566ae86726c060a20c0e
Manuel MILLET <manuel.millet.ovh@gmail.com>
2022-09-25 20:32:48 +0200

    initialisation mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,23 @@
 /* Projet n°6 cours OpenClassrooms "go-fullstack"
-* par Manuel MILLET le 22 septembre 2022 à 12h00
+*  par Manuel MILLET le 25 septembre 2022 à 20h00
+*  fichier app.js
 */
+
+// Déclaration des variables
+// EXPRESS
 const express = require('express');
 const app = express();
-//app.use(express.json());
+app.use(express.json());
+// MONGOOSE
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//************ CORS HEADERS *******/
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -11,78 +25,57 @@ app.use((req, res, next) => {
   next();
 });
 
-
-/*******  middleware *********
-app.use((rep, res, next ) => {
-    console.log('La requête est bien receptionnée !!');
-    next();
-});
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-app.use((req, res, next) => {
-    res.json({ messsage: 'Waowww la requete a bien été reçu !!'});
-    next();
-});
-app.use((req, res) => {
-    console.log('La réponse à été envoyée avec succès !!!');
-})
-*/
-/*
-
+//*******  Middlewares *********
 
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Objet créé !!!'
-    });
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
 });
 
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
-*/
+  const stuff = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'Mon premier objet',
+      description: 'Les infos de mon premier objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 4900,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 2900,
+      userId: 'qsomihvqios',
+    },
+  ];
+  res.status(200).json(stuff);
+});
 
-
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
-
+// fin
 module.exports = app;
+
+/* Pour test de la connexion serveur
+app.use((rep, res, next ) => {
+  console.log('La requête est bien receptionnée !!');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ messsage: 'Waowww la requete a bien été reçu !!' });
+  next();
+});
+
+app.use((req, res) => {
+  console.log('La réponse à été envoyée avec succès !!!');
+});
+*/
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,29 @@
-/* Projet n°6 cours OpenClassrooms "go-fullstack"
-* par Manuel MILLET le 22 septembre 2022 à 12h00
+/* Cours OpenClassrooms "go-fullstack"
+*  lié au projet n°6 PIIQUANTE
+*  par Manuel MILLET le 22 septembre 2022 à 12h00
+*  fichier server.js
 */
+
+//console.log('le server est bien en attente de connexion !');
+// Déclaration des variables
 const http = require('http');
 const app = require('./app');
 const normalizePort = val => {
-    const port = parseInt(val, 10);
-    if (isNaN(port)) {
-        return val;
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
 };
+
 const port = normalizePort(process.env.PORT || '3000');
+
 app.set('port', port);
+
 const errorHandler = error => {
   if (error.syscall !== 'listen') {
     throw error;
@@ -43,4 +52,7 @@ server.on('listening', () => {
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
   console.log('Listening on ' + bind);
 });
+
+//Le server écoute
 server.listen(port);
+// FIN
